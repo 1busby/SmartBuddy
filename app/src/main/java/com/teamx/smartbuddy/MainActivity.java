f6d77553f339505474004bbde2d6b94f2e0f4120
Michael Busby <Michael Busby>
2017-03-24 23:37:08 -0400

    Add Fragment with RecycleView

--- app/src/main/java/com/teamx/smartbuddy/MainActivity.java
+++ app/src/main/java/com/teamx/smartbuddy/MainActivity.java
@@ -1,13 +1,27 @@
 package com.teamx.smartbuddy;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.teamx.smartbuddy.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity implements EventFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setTitle("");
+
+        //getActionBar().setTitle("Wednesday, March 24");
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
---
+++ app/src/main/java/com/teamx/smartbuddy/MyEventRecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package com.teamx.smartbuddy;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.teamx.smartbuddy.EventFragment.OnListFragmentInteractionListener;
+import com.teamx.smartbuddy.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyEventRecyclerViewAdapter extends RecyclerView.Adapter<MyEventRecyclerViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public MyEventRecyclerViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_event, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mEventView.setText(mValues.get(position).event);
+        holder.mTimeView.setText(mValues.get(position).time);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mEventView;
+        public final TextView mTimeView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.id);
+            mEventView = (TextView) view.findViewById(R.id.event);
+            mTimeView = (TextView) view.findViewById(R.id.time);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mEventView.getText() + "'";
+        }
+    }
+}
